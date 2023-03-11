63ca655c86480aa5be4eea21b836c8de89655e54
Rodgers Biwott <rodgersbiwott2016@gmail.com>
2023-03-11 13:55:44 +0300

    Deployment-Updated Django settings and tested deployment locally

--- app/app/settings.py
+++ app/app/settings.py
@@ -20,12 +20,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-n5don_$b8+xn%21y8mqd0l*4j-i#s884%l@xl_4&*cawn$fd&9'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'changeme')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(int(os.environ.get('DEBUG', 0)))
 
 ALLOWED_HOSTS = []
+ALLOWED_HOSTS.extend(
+    filter(None, os.environ.get('ALLOWED_HOSTS', '').split(','))
+)
 
 
 # Application definition
